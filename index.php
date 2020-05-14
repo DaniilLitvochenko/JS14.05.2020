2f6239b964feec77ae250e45299e4a24c1b43e03
DaniilLitvochenko <danay2000@inbox.ru>
2020-05-14 13:32:43 +0300

    Проверка

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
-    require 'vendor/autoload.php';
-    use Carbon\Carbon;
-    echo '<h1>Litovchenko Daniil</h1>';
-    $date = Carbon::now() -> format('j.m.Y h:i');
-    echo '<h2>' . $date . '</h2>';
+        require 'vendor/autoload.php';
+        use Carbon\Carbon;
+    
+        echo '<h1>Litovchenko Daniil</h1>';
+        echo '<h3>Задание 1</h3>';
+        $date = Carbon::now() -> format('j.m.Y h:i');
+        echo '<h2>' . $date . '</h2>';
+    
+        echo '<h3>Задание 2</h3>';
+        echo '<h1>Введите количество рублей</h1>';
+        echo '<input name="data" type="text">';
+        $roubles = 'data';
+        echo $roubles;
+        ?>
